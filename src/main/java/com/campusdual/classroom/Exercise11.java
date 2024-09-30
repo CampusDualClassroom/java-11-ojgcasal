1836672946a260986dde3e08cb90867d591c9e5e
Orlando Jose Garces Casal <ojgcasal@gmail.com>
2024-09-30 11:10:13 +0200

    Ejercicio11 terminado

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,22 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+        blackRemote.turnOn();
+        blackRemote.channelUp();
+        blackRemote.channelDown();
+        blackRemote.channelDown();
+        blackRemote.volumeUp();
+        blackRemote.volumeDown();
+        for (int i = 0; i < 21; i++) {
+            blackRemote.volumeDown();
+        }
+        System.out.println(blackRemote.getColor());
+        blackRemote.turnOff();
+
+
+
+
+
     }
 
 }
